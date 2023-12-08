af7600be2074b20641daa19b345ddb195f90716d
Radu-Maris <raducumaris@gmail.com>
2023-12-09 00:50:33 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
+import 'package:polihack2023/views/start/start_interface.dart';
+ import 'firebase_options.dart'; // Generated file
+ import 'package:firebase_core/firebase_core.dart'; // 
+ import 'package:cloud_firestore/cloud_firestore.dart'; // 
+
+
 import 'package:flutter/material.dart';
+//import 'views/start/start_interface.dart';
 
-import 'views/start/start_interface.dart';
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(options: 
+      DefaultFirebaseOptions.currentPlatform);
+   runApp(const MyApp());
 
-void main() => runApp(const MyApp());
+   final db = FirebaseFirestore.instance;
+
+   final collection = db.collection('/users');
+   collection.doc("radu").set({
+    "key": "value"
+   });
+  }
