1ba684cdab6de53828579ceaaa8dd424f034a42d
antoniopotra <antonio.potra@gmail.com>
2023-12-09 11:48:17 +0200

    flutter-flow-ui

--- android/app/src/main/java/com/example/polihack2023/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.polihack2023;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/models/user.dart
+++
@@ -1,5 +0,0 @@
-class User {
-  final String name;
-
-  User({this.id, required this.name});
-}
